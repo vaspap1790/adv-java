7423018571eaebf66f739bc94621ec72880b6411
vaspap1790 <vaspap1790@gmail.com>
2024-01-14 11:54:42 +0100

    Set up

---
+++ src/main/java/com/example/advjava/AdvJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.advjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/advjava/AdvJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
