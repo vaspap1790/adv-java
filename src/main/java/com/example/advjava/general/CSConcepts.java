b910fb30a697730318731d34e59bb814afcea8cc
Vasilis Papadimitrakopoulos <vasilis.papadimitrakopoulos@ocado.com>
2024-01-31 17:30:59 +0100

    SOLID and OOP

---
+++ src/main/java/com/example/advjava/general/CSConcepts.java
@@ -0,0 +1,40 @@
+package com.example.advjava.general;
+
+public class CSConcepts {
+
+    /**
+     * Parallelism:
+     */
+
+    /**
+     * Concurrency:
+     */
+
+    /**
+     * Multithreading:
+     */
+
+    /**
+     * Multitasking:
+     */
+
+    /**
+     * Multiprocessing:
+     */
+
+    /**
+     * Asynchronous Programming:
+     */
+
+    /**
+     * Dependency Injection
+     * Makes a class independent of its dependencies.
+     * It achieves that by decoupling the usage of an object from its creation.
+     * Helps you to follow SOLID's dependency inversion and single responsibility principles
+     * This way dependencies can be used and tested independently
+     * Three types:
+     * - Constructor Injection (Recommended)
+     * - Setter Injection (hidden dependency may lead to null pointers)
+     * - Field Injection (same problems apply + adds complexity because needs mutation or reflection)
+     */
+}
